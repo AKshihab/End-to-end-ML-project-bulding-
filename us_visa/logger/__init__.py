8dd77523058be4ae3babef4a3ef837e26f679d99
AKshihab <shihabkaiyumhossen@gmail.com>
2026-05-02 16:41:49 +0600

    logger feature added

---
+++ us_visa/logger/__init__.py
@@ -0,0 +1,18 @@
+import logging
+import os
+from datetime import datetime
+from from_root import from_root
+# Create a logger
+LOG_FILE =f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
+
+log_dir="logs"
+
+LOG_directory_PATH=os.path.join(from_root(),log_dir)
+LOG_PATH=os.path.join(LOG_directory_PATH,LOG_FILE)
+os.makedirs(LOG_directory_PATH,exist_ok=True)
+
+logging.basicConfig(
+    filename=LOG_PATH,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    level=logging.DEBUG
+)
